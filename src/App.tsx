735b92c036fdbbf0d210dafe612766bdaa353232
GopiS2001 <165753372+GopiS2001@users.noreply.github.com>
2026-06-24 18:47:43 +0530

    auto: update App.tsx [2026-06-24 18:47]

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,17 @@ import SignIn from "./pages/AuthPages/SignIn";
 import SignUp from "./pages/AuthPages/SignUp";
 import NotFound from "./pages/OtherPage/NotFound";
 import UserProfiles from "./pages/UserProfiles";
-import Videos from "./pages/UiElements/Videos";
-import Images from "./pages/UiElements/Images";
-import Alerts from "./pages/UiElements/Alerts";
-import Badges from "./pages/UiElements/Badges";
-import Avatars from "./pages/UiElements/Avatars";
-import Buttons from "./pages/UiElements/Buttons";
-import LineChart from "./pages/Charts/LineChart";
-import BarChart from "./pages/Charts/BarChart";
-import Calendar from "./pages/Calendar";
-import BasicTables from "./pages/Tables/BasicTables";
-import FormElements from "./pages/Forms/FormElements";
-import Blank from "./pages/Blank";
 import AppLayout from "./layout/AppLayout";
 import { ScrollToTop } from "./components/common/ScrollToTop";
 import Home from "./pages/Dashboard/Home";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Users from "./pages/Users/Users";
 import Students from "./pages/Students/Students";
 import Batches from "./pages/Batches/Batches";
 import Staff from "./pages/Staff/Staff";
+import ClassStudents from "./pages/Classes/ClassStudents";
+import Reports from "./pages/Reports/Reports";
 
 export default function App() {
   return (
@@ -37,32 +28,14 @@ export default function App() {
                 <Route index path="/" element={<Home />} />
 
                 {/* Tuition Centre Management */}
-                <Route path="/students" element={<Students />} />
+                <Route path="/users" element={<Users />} />
                 <Route path="/batches" element={<Batches />} />
+                <Route path="/students" element={<Students />} />
                 <Route path="/staff" element={<Staff />} />
+                <Route path="/classes/:classNumber" element={<ClassStudents />} />
+                <Route path="/reports" element={<Reports />} />
 
-                {/* Others Page */}
                 <Route path="/profile" element={<UserProfiles />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/blank" element={<Blank />} />
-
-                {/* Forms */}
-                <Route path="/form-elements" element={<FormElements />} />
-
-                {/* Tables */}
-                <Route path="/basic-tables" element={<BasicTables />} />
-
-                {/* Ui Elements */}
-                <Route path="/alerts" element={<Alerts />} />
-                <Route path="/avatars" element={<Avatars />} />
-                <Route path="/badge" element={<Badges />} />
-                <Route path="/buttons" element={<Buttons />} />
-                <Route path="/images" element={<Images />} />
-                <Route path="/videos" element={<Videos />} />
-
-                {/* Charts */}
-                <Route path="/line-chart" element={<LineChart />} />
-                <Route path="/bar-chart" element={<BarChart />} />
               </Route>
             </Route>
 
